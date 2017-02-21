d84fb3d9d51d9165a50832c629d337ba91ca2781
Connie Zabarovskaya <conny.zest@gmail.com>
2017-02-20 21:03:17 -0600

    reviewing ArrayList API, autoboxing, etc.

--- src/morepractice/JavaExam.java
+++ src/morepractice/JavaExam.java
@@ -50,27 +50,63 @@ public class JavaExam {
 		
 		// List is an interface, so you can assign ArrayList object to it, but not vice versa.
 		// Generics are optional here, but if you don't specify them, it defaults to Object
-		List<Circus> townCircuses = new ArrayList<>();
+		// You can optionally provide initial capacity for the ArrayList, but you don't  have to.
+		List<Circus> townCircuses = new ArrayList<>(1);
 		// This won't compile, because we declared this to be a String ArrayList only
 		//townCircuses.add("InvalidCircus");
+		
+		// This constructor creates a list containing the elements of the specified 
+		// collection, in the order they are returned by the collection's iterator.
+		// Notice that it creates a brand new object, not reference to passed object
+		ArrayList<Circus> townCircuses2 = new ArrayList<>(townCircuses);
+		
+		// Compare two lists
+		// to see if they contain the same elements in the same order.
+		System.out.println("Both arrayLists are the same?: " + townCircuses2.equals(townCircuses));
+		
+		// Add an object to second list
+		townCircuses2.add(j.new Circus());
+		System.out.println("townCircuses empty? " + townCircuses.isEmpty()); // true
+		System.out.println("townCircuses2 empty? " + townCircuses2.isEmpty()); // false
+		System.out.println("Both arrayLists are the same?: " + townCircuses2.equals(townCircuses));
+
 		
 		String g = "grep";
 		String p = g.replace("gr", "2two");
 		System.out.println(p);
 		String a = new String();
-		System.out.println(new String());
+		System.out.println("New String object: " + new String());
 		System.out.println(a.replace("", "b"));
+		// Character.parseChar("a"); // There is no method parseChar(String s)
+		Integer wildNumber = Integer.valueOf(34); // Integer.valueOf takes only int or String. Throws exception if String has letters and punctuation
+		System.out.println(wildNumber);
+		
+		System.out.println(Boolean.valueOf("True"));
+
 		
 		ArrayList magicianObjects = new ArrayList(); // this will only allow Objects, and any primitives will be autoboxed - promoted to wrapper classes
 		magicianObjects.add("AlbinoRabbit");  
 		magicianObjects.add(Boolean.TRUE); // this adds an object of wrapper class
 		// The max index for adding an element is equal to size of array, otherwise, it will throw exception at runtime.
 		magicianObjects.add(2, 42);
+		magicianObjects.add(Boolean.TRUE); // adding a duplicate works
+		System.out.println("Find true: " + magicianObjects.contains(true));
 		System.out.println(magicianObjects);
+		System.out.println(magicianObjects.remove(Integer.valueOf(42))); // proper way to remove int from ArrayList, because valueOf returns wrapper class
 		//System.out.println(magicianObjects.remove(42)); // this will attempt to remove the element at index 42 (no autoboxing rules for "remove")
-		System.out.println(magicianObjects.remove(true)); // removes the Boolean object
-		System.out.println(magicianObjects.remove("BlackRabbit"));
+		System.out.println(magicianObjects.remove(true)); // removes only the first match of Boolean object
+		System.out.println(magicianObjects.remove("BlackRabbit")); // false (no match found)
 		System.out.println(magicianObjects.size());
+		magicianObjects.set(0, false); // replacing an existing object with a primitive
+		System.out.println(magicianObjects); 
+		System.out.println(magicianObjects.remove(false)); // true
+		// This will clear out all elements, returning size to 0
+		// magicianObjects.clear();
+		
+		// Contains method calls equals() on every element of array, that allows primitives
+		// to be recognized and properly compared
+		System.out.println("Find 42: " + magicianObjects.contains(42));
+		
 
 		
 	}
